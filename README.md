96a11068f86c17783ce20f1368a8a7aadaa9e04b
G. Queiroz <gabrieljvnq@gmail.com>
2021-01-08 01:01:00 -0300

    Added bootstrap and vue.js

--- README.md
+++ README.md
@@ -12,4 +12,6 @@ A "common base" for Web Apps.
     * Database credentials
     * Storing other config info
 
+## Quirks
 
+  * Each user can have multiple usernames. (this allows login via email, SSN, computer username, etc)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::helmet::SpaceHelmet;
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+struct BasicCtx {
+    page_title: String,
+    page_desc: Option<String>
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/login")]
+fn login() -> Template {
+    let context = BasicCtx { page_title: "abc".to_string(), page_desc: Some("dsds".to_string()) };
+    Template::render("login", &context)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    let helmet = SpaceHelmet::default();
+    rocket::ignite()
+        .attach(helmet)
+        .attach(Template::fairing())
+        .mount("/static", StaticFiles::from("static"))
+        .mount("/", routes![index, login])
+        .launch();
 }
