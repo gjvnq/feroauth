157597df0379b8579e127e2cc6fe6ab029aff4fa
G Queiroz <gabrieljvnq@gmail.com>
2021-01-11 23:47:05 -0300

    Improvements on login page

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 mod prelude;
+use rocket::request::Form;
 use crate::prelude::*;
 
 #[macro_use] extern crate rocket;
@@ -11,11 +12,34 @@ use rocket_contrib::helmet::SpaceHelmet;
 use rocket_contrib::templates::Template;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct BasicCtx {
     disable_vue: bool,
     page_title: String,
     page_desc: Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginPageCtx {
+    base: BasicCtx,
+    username: String,
+    stage: LoginStage,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum LoginStage {
+    AskUsername,
+    AskPassword,
+    AskSelect2FA,
+    AskOTP,
+    AskU2F,
+}
+
+#[derive(Debug, FromForm)]
+struct LoginFormInput {
+    username: Option<String>,
+    password: Option<String>,
+    otp: Option<String>
 }
 
 #[get("/")]
@@ -23,10 +47,27 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/login")]
-fn login() -> Template {
-    let context = BasicCtx { disable_vue: true, page_title: "abc".to_string(), page_desc: Some("dsds".to_string()) };
-    Template::render("login", &context)
+#[get("/login?<username>")]
+fn login1(username: Option<String>) -> Template {
+    let base_ctx = BasicCtx { disable_vue: true, page_title: "abc".to_string(), page_desc: Some("dsds".to_string()) };
+    let ctx = LoginPageCtx {
+        base: base_ctx,
+        username: username.unwrap_or_default(),
+        stage: LoginStage::AskUsername
+    };
+    Template::render("login", &ctx)
+}
+
+
+#[post("/login", data = "<input>")]
+fn login2(input: Form<LoginFormInput>) -> Template {
+    let base_ctx = BasicCtx { disable_vue: true, page_title: "abc".to_string(), page_desc: Some("dsds".to_string()) };
+    let ctx = LoginPageCtx {
+        base: base_ctx,
+        username: input.username.clone().unwrap_or_default(),
+        stage: LoginStage::AskPassword
+    };
+    Template::render("login", &ctx)
 }
 
 fn main() {
@@ -35,6 +76,6 @@ fn main() {
         .attach(helmet)
         .attach(Template::fairing())
         .mount("/static", StaticFiles::from("static"))
-        .mount("/", routes![index, login])
+        .mount("/", routes![index, login1, login2])
         .launch();
 }
