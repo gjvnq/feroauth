457d14a54b527d510de920887bdcbb58d1c6267b
G. Queiroz <gabrieljvnq@gmail.com>
2021-04-11 23:08:28 -0300

    Started using a proxy for the API when testing

--- wui/vue.config.js
+++ wui/vue.config.js
@@ -1,29 +1,40 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
-    }
-  },
+	pluginOptions: {
+		i18n: {
+			locale: 'en',
+			fallbackLocale: 'en',
+			localeDir: 'locales',
+			enableInSFC: true
+		}
+	},
 
-  assetsDir: 'static',
+	devServer: {
+		proxy: {
+			"^/api": {
+				ws: true,
+				changeOrigin: true,
+				pathRewrite: {'^/api': ''},
+				target: process.env.VUE_APP_API_ADDR,
+			}
+		}
+	},
 
-  css: {
-    sourceMap: true
-  },
+	assetsDir: 'static',
 
-  pages: {
-    login: {
-      entry: 'src/main-login.js',
-      template: 'public/login.html',
-      filename: 'login.html'
-    },
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-    } 
-  }
+	css: {
+		sourceMap: true
+	},
+
+	pages: {
+		login: {
+			entry: 'src/main-login.js',
+			template: 'public/login.html',
+			filename: 'login.html'
+		},
+		index: {
+			entry: 'src/main.js',
+			template: 'public/index.html',
+			filename: 'index.html',
+		} 
+	}
 }
